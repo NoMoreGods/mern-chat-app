b89ed5ed79237da6e9bd76d9f9c3454c3e09cf37
NoMoreGods <nomoregods@bk.ru>
2024-03-22 01:00:37 +0300

    Добавил навигацию через useAuthContext

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useContext, useState } from 'react';
 
 export const AuthContext = createContext();
+
 // eslint-disable-next-line react-refresh/only-export-components
 export const useAuthContext = () => {
   return useContext(AuthContext);
@@ -8,5 +9,6 @@ export const useAuthContext = () => {
 
 export const AuthContextProvider = ({ children }) => {
   const [authUser, setAuthUser] = useState(JSON.parse(localStorage.getItem('chat-user')) || null);
+
   return <AuthContext.Provider value={{ authUser, setAuthUser }}>{children}</AuthContext.Provider>;
 };
